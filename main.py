e109f2194476b6af6471d424ccaa3332b688d58b
Victor Njoroge <victornjoroge2020@gmail.com>
2023-09-07 17:03:19 +0300

    implemented functions

--- main.py
+++ main.py
@@ -12,183 +12,154 @@ session = Session()
 
 Base.metadata.create_all(engine)
 
-def main():
-    print("Welcome to the BnB booking system!")
-
-    choice = int(input("Choose your action:\n1: Login\n2: Signup\n"))
-
-    if choice == 1:
-        # Login
-        login_choice = int(input("Choose your role:\n1: Customer\n2: Owner\n"))
-
+def Customer_login(login_choice):
+    if login_choice == 1:
         phone_no = int(input("Enter your phone number: "))
         password = input("Enter your password: ")
-
-        if login_choice == 1:
-            # Customer login
-            customer = session.query(Customer).filter_by(phoneNumber=phone_no, passWord=password).first()
-            if customer:
-                print("Customer login successful!")
-
-                # Option to book a BnB
-                book_choice = int(input("Choose an action:\n1: Book a BnB\n2: Log out\n"))
-
-                if book_choice == 1:
-                    # Booking a BnB
-                    location = input("Enter your desired location: ")
-
-                    matching_bnbs = session.query(Bnb).filter_by(location=location).all()
-
-                    if not matching_bnbs:
-                        print(f"No BnBs found in location: {location}")
-                    else:
-                        print("Matching BnBs:")
-                        for idx, bnb in enumerate(matching_bnbs, start=1):
-                            print(f"{idx}: {bnb.name}, Address: {bnb.address}, Price: {bnb.price}")
-
-                        choice = int(input("Enter the number of the BnB you want to book: "))
-
-                        if 1 <= choice <= len(matching_bnbs):
-                            selected_bnb = matching_bnbs[choice - 1]
-                            checkin = input("Enter Check-In Date in this format YYYY-MM-DD: ")
-                            checkout = input("Enter Check-Out Date in this format YYYY-MM-DD: ")
-
-                            try:
-                                check_in = datetime.strptime(checkin, "%Y-%m-%d").date()
-                                check_out = datetime.strptime(checkout, "%Y-%m-%d").date()
-                            except ValueError:
-                                print("Invalid date format. Please use YYYY-MM-DD.")
-                                return
-
-                            new_booking = Booking(customer=customer, bnb=selected_bnb, check_in=check_in, check_out=check_out)
-                            session.add(new_booking)
-                            session.commit()
-                            print("Booking successful!")
-
-                        else:
-                            print("Invalid BnB selection.")
-
-                elif book_choice == 2:
-                    print("Logging out.")
-
-            else:
-                print("Invalid login credentials for Customer.")
-
-        elif login_choice == 2:
-            # Owner login
-            owner = session.query(Owner).filter_by(phoneNumber=phone_no, passWord=password).first()
-            if owner:
-                print("Owner login successful!")
-
-                # Option to register a BnB
-                register_choice = int(input("Choose an action:\n1: Register a BnB\n2: Log out\n"))
-
-                if register_choice == 1:
-                    # Registering a BnB
-                    location = input("Enter the location of your BnB: ")
-                    address = input("Enter the address of your BnB: ")
-                    price = int(input("Enter the price per night: "))
-                    name = input("Enter the name of your BnB: ")
-                    status = input("Enter the status of your BnB: ")
-
-                    new_bnb = Bnb(owner=owner, location=location, address=address, price=price, name=name, status=status)
-                    session.add(new_bnb)
-                    session.commit()
-                    print("BnB registration successful!")
-
-                elif register_choice == 2:
-                    print("Logging out.")
-
-            else:
-                print("Invalid login credentials for Owner.")
-
+        customer = session.query(Customer).filter_by(phoneNumber=phone_no, passWord=password).first()
+         
+        if customer:
+            print(f"Welcome Back {customer.firstName} {customer.lastName}")
+            booking(customer)
         else:
-            print("Invalid role choice for login.")
+            print("Invalid login credentials for Customer.")
 
-    elif choice == 2:
-        # Signup
-        signup_choice = int(input("Choose your role:\n1: Customer\n2: Owner\n"))
+def Customer_account_creation(creation_choice):
+    if creation_choice == 1:
+        fname=input("Enter your First Name: ")
+        lname=input("Enter your Sir Name: ")
+        phoneNo=int(input("Enter your Phone Number: "))
+        passWord=input("Enter your password: ")
 
-        fname = input("Enter first name: ")
-        lname = input("Enter last name: ")
-        phone_no = int(input("Enter phone number: "))
-        password = input("Enter password, the password must be 8 characters: ")
+        # check if Customer exists
 
-        if signup_choice == 1:
-            # Customer registration
-            existing_customer = session.query(Customer).filter_by(phoneNumber=phone_no).first()
-            if existing_customer:
-                print("Customer already exists. Please log in.")
-            else:
-                new_customer = Customer(firstName=fname, lastName=lname, phoneNumber=phone_no, passWord=password)
-                session.add(new_customer)
-                session.commit()
-                print("Customer registration successful!")
+        existing_customer=session.query(Customer).filter_by(phoneNumber=phoneNo).first()
 
-                # Proceed to book a BnB
-                location = input("Enter your desired location: ")
+        if existing_customer:
+            print("User already exists")
+        else :
+            customer = Customer(firstName=fname, lastName=lname, phoneNumber=phoneNo, passWord=passWord)
+            session.add(customer)
+            session.commit()
+            print("Customer registration successful!")
+            booking(customer)
 
-                matching_bnbs = session.query(Bnb).filter_by(location=location).all()
 
-                if not matching_bnbs:
-                    print(f"No BnBs found in location: {location}")
-                else:
-                    print("Matching BnBs:")
-                    for idx, bnb in enumerate(matching_bnbs, start=1):
-                        print(f"{idx}: {bnb.name}, Address: {bnb.address}, Price: {bnb.price}")
 
-                    choice = int(input("Enter the number of the BnB you want to book: "))
+def booking(customer):
+    booking_choice=int(input("Do you want to make booking ? \n 1: Yes \n 2: No \n"))
 
-                    if 1 <= choice <= len(matching_bnbs):
-                        selected_bnb = matching_bnbs[choice - 1]
-                        checkin = input("Enter Check-In Date in this format YYYY-MM-DD: ")
-                        checkout = input("Enter Check-Out Date in this format YYYY-MM-DD: ")
+    if booking_choice == 1:
+        location=input("Enter desired destination ? ")
 
-                        try:
-                            check_in = datetime.strptime(checkin, "%Y-%m-%d").date()
-                            check_out = datetime.strptime(checkout, "%Y-%m-%d").date()
-                        except ValueError:
-                            print("Invalid date format. Please use YYYY-MM-DD.")
-                            return
+        matching_location=session.query(Bnb).filter_by(location=location).all()
 
-                        new_booking = Booking(customer=new_customer, bnb=selected_bnb, check_in=check_in, check_out=check_out)
-                        session.add(new_booking)
-                        session.commit()
-                        print("Booking successful!")
-
-                    else:
-                        print("Invalid BnB selection.")
-
-        elif signup_choice == 2:
-            # Owner registration
-            existing_owner = session.query(Owner).filter_by(phoneNumber=phone_no).first()
-            if existing_owner:
-                print("Owner already exists. Please log in.")
-            else:
-                new_owner = Owner(firstName=fname, lastName=lname, phoneNumber=phone_no, passWord=password)
-                session.add(new_owner)
-                session.commit()
-                print("Owner registration successful!")
-
-                # Proceed to register a BnB
-                location = input("Enter the location of your BnB: ")
-                address = input("Enter the address of your BnB: ")
-                price = int(input("Enter the price per night: "))
-                name = input("Enter the name of your BnB: ")
-                status = input("Enter the status of your BnB: ")
-
-                new_bnb = Bnb(owner=new_owner, location=location, address=address, price=price, name=name, status=status)
-                session.add(new_bnb)
-                session.commit()
-                print("BnB registration successful!")
-
-        else:
-            print("Invalid role choice for signup.")
-
+        making_bookings(matching_location,location,customer)
+    elif booking_choice == 2:
+        print("Logging  Out")
     else:
-        print("Invalid action choice. Please choose 1 for Login or 2 for Signup.")
+        print("Enter valid choice")
 
-    session.close()
+def making_bookings(matching_location,location, customer):
+    if not matching_location:
+            print(f"No bns found in this location {location}")
+    else:
+        print(f"Here are the Bnbs in {location}")
+        for idx, bnb in enumerate(matching_location, start=1):
+            print(f"{idx}, Name: {bnb.name} Address: {bnb.address}, Price: {bnb.price}, ")
+
+        choice=int(input("Enter the number for the Bnb of your choocing"))
+
+        if 1 <= choice <= len(matching_location):
+            selected=matching_location[choice-1]
+            checkin = input("Enter Check-In Date in this format YYYY-MM-DD: ")
+            checkout = input("Enter Check-Out Date in this format YYYY-MM-DD: ")
+
+            try:
+                check_in = datetime.strptime(checkin, "%Y-%m-%d").date()
+                check_out = datetime.strptime(checkout, "%Y-%m-%d").date()
+            except ValueError:
+                print("Invalid date format. Please use YYYY-MM-DD.")
+                return
+
+            new_booking = Booking(customer=customer, bnb=selected, check_in=check_in, check_out=check_out)
+            session.add(new_booking)
+            session.commit()
+            print("Booking successful!")
+
+
+            selected.status="booked"
+            session.commit()
+        else:
+            print("You have entered invalid bnb selection")
+
+
+def Owner_login(login_choice):
+    if login_choice == 2:
+        phone_no = int(input("Enter your phone number: "))
+        password = input("Enter your password: ")
+        owner = session.query(Owner).filter_by(phoneNumber=phone_no, passWord=password).first()
+
+
+        if owner:
+            print(f"Welcome back {owner.firstName} {owner.lastName}")
+            
+            register_bnb(owner)
+        else :
+            print("Invalid credentials !!")
+
+def Owner_register(creation_choice):
+    
+    if creation_choice == 2:
+        fname=input("Enter your First Name: ")
+        lname=input("Enter your Sir Name: ")
+        phoneNo=int(input("Enter your Phone Number: "))
+        passWord=input("Enter your password: ")
+
+        existing_owner=session.query(Owner).filter_by(phoneNumber=phoneNo).first()
+
+        if existing_owner:
+            print("User already exists")
+
+        else:
+            owner=Owner(firstName=fname, lastName=lname, phoneNumber=phoneNo, passWord=passWord)
+            session.add(owner)
+            session.commit()
+            print("Owner registration successful!")
+            register_bnb(owner)
+
+
+def register_bnb(owner):
+    choice=int(input("Do you want to register a Bnb \n 1: Yes \n 2: No \n"))
+    if choice == 1:
+        location = input("Enter the location of your BnB: ")
+        address = input("Enter the address of your BnB: ")
+        price = int(input("Enter the price per night: "))
+        name = input("Enter the name of your BnB: ")
+        status = input("Enter the status of your BnB: ")
+
+        new_bnb = Bnb(owner=owner, location=location, address=address, price=price, name=name, status=status)
+        session.add(new_bnb)
+        session.commit()
+        print("BnB registration successful!")
+    else:
+        print("Logging Out")
+
+
+def main():
+    print("Welcome to Bnb booking system!")
+
+    choice = int(input("Choose your action:\n1: Sign in\n2: Sign up\n"))
+
+    if choice == 1:
+        login_choice = int(input("Choose your role:\n1: Customer\n2: Owner\n"))
+        Customer_login(login_choice)
+        Owner_login(login_choice)
+    elif choice == 2:
+        creation_choice=int(input("Choose your role: \n1: Customer \n 2: Owner \n"))
+        Customer_account_creation(creation_choice)
+        Owner_register(creation_choice)
+
 
 if __name__ == "__main__":
     main()
